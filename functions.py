37dce59ef03be13c9defbf40543ee60dba58e3a1
Joel Medeiros <joel.joao.medeiros@gmail.com>
2022-10-04 18:14:44 -0300

    initial commit

---
+++ functions.py
@@ -0,0 +1,45 @@
+import json
+
+from pkg_resources import working_set
+
+removeCaracteres = ('.', ',', '!', '?', ':', ';', "{", "}", "(", ")")
+stopedWords = ("NO", "NA", "O", "A", "E", "À", "ÀS", "DA", "SEM", "DE", "DAS", "DOS", "DO", "SE", "AS", "UM", "UMA", "UNS", "UMAS", "AO", "NA", "NOS", "MAS", "OS", "EM", "PARA", 'É', 'QUE', 'AOS')
+
+def readJson(filepatch):
+    file = open(filepatch, 'r')
+    return json.load(file)
+
+def cleanCaracteres(bible):
+    
+    bigString = ''
+    versiclesList = []
+    for book in bible:
+        chapters = book["chapters"]
+    
+        for chapter in chapters:
+            for versicle in chapter:
+                count = 0
+                listVersicle = list(versicle)
+
+                for letter in listVersicle:
+
+                    if letter in removeCaracteres:
+                        listVersicle[count] = ''
+                    else: pass   
+
+                    count += 1
+
+                versiclesList.append(''.join(listVersicle))
+
+    bigString = ' '.join(versiclesList)
+
+    return bigString.upper()
+
+def cleanStoppedWords(text):
+    
+    if text in stopedWords:
+        flag = False
+    else:
+        flag = True
+    
+    return flag
---
+++ main.py
@@ -0,0 +1,19 @@
+from operator import index
+from time import sleep, time
+from functions import *
+initTime = time()
+
+versions = ("nvi", "acf", "aa")
+
+version = "aa"
+filepatch = f"json/{version}.json"
+bible = readJson(filepatch)
+bigString = cleanCaracteres(bible)
+bigList = bigString.split(' ')
+filterList = filter(cleanStoppedWords, bigList)
+bigCleanString = ' '.join(filterList)
+print(bigCleanString)
+
+
+endTime = time()
+print(endTime-initTime)
